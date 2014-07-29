fd5a3396d2ebe928536de4f016c6f88d7d1b2354
Dan Burke <dburke@addictmud.org>
2014-07-29 10:42:04 -0400

    tweak example names

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ docker run -d \
 	-p 5672:5672 \
 	-p 15672:15672 \
 	--name=rmq1 \
-	-e RABBITMQ_VHOST=addict-dev \
-	-e RABBITMQ_USER=addict \
+	-e RABBITMQ_VHOST=appname-dev \
+	-e RABBITMQ_USER=appuser \
 	-e RABBITMQ_PASS=abc123 \
 	dwburke/centos-rabbitmq
 

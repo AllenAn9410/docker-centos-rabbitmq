1192b8593a84bce909f014a5a778638f3440025a
Dan Burke <dburke@addictmud.org>
2014-12-06 18:03:46 -0500

    test run script

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run -t -i --rm=true \
+	-p 5672:5672 \
+	-p 15672:15672 \
+	--name=rmq1 \
+	-e RABBITMQ_VHOST=dev \
+	-e RABBITMQ_USER=appuser \
+	-e RABBITMQ_PASS=abc123 \
+	dwburke/centos-rabbitmq
+
+
